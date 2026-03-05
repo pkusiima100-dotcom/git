2c1f82332bd5d0e1f19a425e10fabb465999c918
pkusiima100-dotcom <pkusiima100@gmail.com>
2026-03-05 07:51:24 -0500

    innitialcommit

---
+++ Student.java
@@ -0,0 +1,12 @@
+public class Student{
+    public static void main(String[] args){
+        int age = 18;
+        String regno = "25/u/04315/PS";
+        String name = "Jackline";
+
+    System.out.println("My name is " + name);
+    System.out.println("I am " + age + "old");
+    System.out.println("My registration number is "+ regno );
+
+    }
+}
